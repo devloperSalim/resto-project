b7d536cc44c6a8a7bf87bc275abac604f2969ba2
boulkhir salim <salimboulkhir123@gmail.com>
2024-02-18 17:10:50 +0100

    created database

---
+++ app/Http/Controllers/ServantController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Servant;
+use Illuminate\Http\Request;
+
+class ServantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Servant  $servant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Servant $servant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Servant  $servant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Servant $servant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Servant  $servant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Servant $servant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Servant  $servant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Servant $servant)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_02_18_155705_create_sale_table_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_table', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('table_id')->unsigned();
+            $table->bigInteger('sales_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('table_id')
+                ->references('id')
+                ->on('tables')
+                ->onDelete('cascade');
+
+            $table->foreign('sales_id')
+                ->references('id')
+                ->on('sales')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_table');
+    }
+};
